769e62d0d7b9d94757b482d1a854af792fc1300e
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2021-08-02 13:30:31 -0500

    agregando indice

--- clase-1/main.js
+++ clase-1/main.js
@@ -1,17 +1,29 @@
-const additionButton = document.getElementById('addition-button');
-
-let dataTable = [];
-
-additionButton.addEventListener('click',()=>{
-
-  const numbers = getValues('number-1-input','number-2-input');
-  const additionResult = addition(numbers);
-  dataTable.push(additionResult);
-  console.log(dataTable);
-
-  printTable('table-results',dataTable);
-
-});
-
+//array
+let letters = ['A','B','C'];
+//object
+let person = {
+  name:'Fernando',
+  firstLastname:'Acosta',
+  secondLastname:'Torres',
+  birthDate:'1998-03-20',
+  email:'facostatorres34@gmail.com'
+};
+//table
+let fruits = [
+  {id:1,name:'apple'},
+  {id:2,name:'orange'},
+  {id:3,name:'lemmon'},
+  {id:4,name:'tangerine'},
+  {id:5,name:'strawberry'},
+  {id:6,name:'blueberry'},
+  {id:7,name:'cherry'}
+];
+//numbers
+let numbers = [
+  [65,81,9,45,87],
+  [85,8,91,41,98],
+  [89,10,56,25,68],
+  [19,9,19,15,18]  
+];
 
 
